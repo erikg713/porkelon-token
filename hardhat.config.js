b536d09e7fe90b4462ba43f98cb24dd0e721e26b
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-07 11:01:39 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,17 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
 
+const { PRIVATE_KEY, SEPOLIA_RPC, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || ""
+  }
+};
