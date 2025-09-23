1d6501e074153d77464dfd0e904586ed743cce6c
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-23 10:17:46 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,37 @@
+// scripts/deploy.js
+const hre = require("hardhat");
+require("dotenv").config();
+
+async function main() {
+  const { ethers } = hre;
+  const walletAddress = (await ethers.getSigners())[0].address;
+  console.log("Deploying from:", walletAddress);
+
+  // Compile
+  await hre.run("compile");
+
+  // Factory
+  const Porkelon = await ethers.getContractFactory("Porkelon");
+
+  // Total supply: 100,000,000,000 tokens with 18 decimals
+  const totalSupply = ethers.utils.parseUnits("100000000000", 18);
+
+  console.log("Deploying Porkelon with total supply:", totalSupply.toString());
+
+  const contract = await Porkelon.deploy(totalSupply);
+  await contract.deployed();
+
+  console.log("Porkelon deployed at:", contract.address);
+  console.log("Done.");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
 const { ethers, upgrades } = require("hardhat");
 require("dotenv").config();
 
