f438f9c847bf529de19193e0e007c974ac5ad0c6
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-26 14:43:31 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+// hardhat.config.js
 require("@nomicfoundation/hardhat-toolbox");
 require("@openzeppelin/hardhat-upgrades");
 require("dotenv").config();
@@ -14,34 +15,21 @@ module.exports = {
     }
   },
   networks: {
-    hardhat: {
-      chainId: 1337,
-      gas: 12000000,
-      blockGasLimit: 12000000
-    },
     amoy: {
       url: process.env.AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 80002,
-      gasPrice: 30000000000
-    },
-    polygon: {
-      url: process.env.POLYGON_RPC_URL || "https://polygon-rpc.com/",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 137,
-      gasPrice: 30000000000
+      gasPrice: 30000000000 // 30 Gwei
     }
   },
   etherscan: {
     apiKey: {
-      polygon: process.env.POLYGONSCAN_API_KEY,
       polygonAmoy: process.env.POLYGONSCAN_API_KEY
     }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS === "true",
-    currency: "USD",
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    currency: "USD"
   },
   mocha: {
     timeout: 60000
