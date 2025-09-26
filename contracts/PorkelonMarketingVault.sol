e5dc6ee0ab7e99a341ab855f79a8f45dcf40f8e0
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-09-26 07:40:59 -0500

    Update PorkelonMarketingVault.sol

--- contracts/PorkelonMarketingVault.sol
+++ contracts/PorkelonMarketingVault.sol
@@ -3,22 +3,50 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+/**
+ * @title PorkelonMarketingVault
+ * @dev Vests PORK tokens for marketing with linear release over 2 years.
+ */
 contract PorkelonMarketingVault is Ownable {
-    IERC20 public token;
-    uint256 public marketingPool;
+    using SafeMath for uint256;
 
-    event MarketingWithdrawn(address indexed to, uint256 amount);
+    IERC20 public immutable token;
+    address public immutable beneficiary;
+    uint256 public immutable startTime;
+    uint256 public immutable duration;
+    uint256 public released;
 
-    constructor(IERC20 _token, uint256 _marketingPool) Ownable(msg.sender) {
+    event TokensReleased(address indexed beneficiary, uint256 amount);
+
+    constructor(IERC20 _token, address _beneficiary, uint256 _duration) Ownable(msg.sender) {
+        require(address(_token) != address(0), "PorkelonVault: Invalid token address");
+        require(_beneficiary != address(0), "PorkelonVault: Invalid beneficiary");
+        require(_duration > 0, "PorkelonVault: Invalid duration");
+
         token = _token;
-        marketingPool = _marketingPool;
+        beneficiary = _beneficiary;
+        startTime = block.timestamp;
+        duration = _duration;
     }
 
-    function withdraw(address to, uint256 amount) external onlyOwner {
-        require(amount <= marketingPool, "Not enough funds");
-        marketingPool -= amount;
-        require(token.transfer(to, amount), "Transfer failed");
-        emit MarketingWithdrawn(to, amount);
+    function release() external onlyOwner {
+        uint256 releasable = vestedAmount().sub(released);
+        require(releasable > 0, "PorkelonVault: No tokens to release");
+        released = released.add(releasable);
+        require(token.transfer(beneficiary, releasable), "PorkelonVault: Transfer failed");
+        emit TokensReleased(beneficiary, releasable);
+    }
+
+    function vestedAmount() public view returns (uint256) {
+        uint256 totalBalance = token.balanceOf(address(this)).add(released);
+        if (block.timestamp < startTime) {
+            return 0;
+        } else if (block.timestamp >= startTime.add(duration)) {
+            return totalBalance;
+        } else {
+            return totalBalance.mul(block.timestamp.sub(startTime)).div(duration);
+        }
     }
 }
