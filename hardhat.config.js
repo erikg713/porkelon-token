3eeb13fc68e0aaba392434c0b7b4812fcd00cacc
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-12-06 05:11:00 -0600

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,33 +1,40 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@typechain/hardhat";
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.19",
-    settings: { optimizer: { enabled: true, runs: 200 } }
-  },
-  paths: {
-    sources: "contracts",
-    tests: "test",
-    cache: "cache",
-    artifacts: "artifacts"
-  },
-  typechain: {
-    outDir: "typechain",
-    target: "ethers-v6"
+    version: "0.8.24",
+    settings: { optimizer: { enabled: true, runs: 200 } },
   },
   networks: {
-    hardhat: {},
-    mumbai: {
-      url: process.env.MUMBAI_RPC || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    },
     polygon: {
-      url: process.env.POLYGON_RPC || "",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    }
-  }
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.PRIVATE_KEY as string],
+      gasPrice: 120000000000,
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY as string,
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY as string,
+    },
+    customChains: [
+      {
+        network: "polygon",
+        chainId: 137,
+        urls: {
+          apiURL: "https://api.polygonscan.com/api",
+          browserURL: "https://polygonscan.com",
+        },
+      },
+    ],
+  },
 };
 
 export default config;
